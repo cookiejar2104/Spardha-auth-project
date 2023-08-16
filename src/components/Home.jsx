ad06eb6d3a8cb6ae075765cdf1255c2262d44a0c
Dibyajyoti Mandal <130833968+cookiejar2104@users.noreply.github.com>
2023-08-16 22:05:17 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
   }
 
   return (
-    <div className='mx-10 my-[100px] md:m-auto md:my-[100px] max-w-[700px] justify-center p-10 shadow-slate-700 shadow-2xl bg-[#242424] rounded-2xl'>
+    <div className='mx-10 my-[100px] md:m-auto md:my-[100px] max-w-[700px] justify-center p-10 shadow-slate-700 shadow-2xl bg-[#242424] rounded-2xl border border-md border-slate-600'>
       <div className='text-white text-2xl justify-center sm:text-3xl font-bold '>Welcome to <span className='text-cyan-500 sm:text-4xl'>Spardha'23</span></div>
 
       <div className='pt-6 sm:text-2xl'>Spardha, IIT (BHU) Varanasi's annual sports fest, boasts 36+ years of glory. A premier Northern India event, it hosts 1000+ athletes competing in diverse sports, showcasing exceptional talent and fervor.</div>
